8681b42f7b3d16dab369ff1e11488aa01dd9c1ff
Ian Frost <inf5000@gmail.com>
2016-05-24 00:06:18 -0400

    General layout of header is down. Pseudo elements are causing issues with parent selector & in CSS

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,10 @@
-$('.sgg-nav__trigger').click(function(event) {
+$('.nav__trigger').click(function(event) {
 	event.preventDefault();
-	$('.sgg-nav__list').toggleClass('sgg-nav__list--active');
+	$('.nav__list').toggleClass('nav__list--active');
+});
+
+$('.nav__item').click(function(event) {
+	$('.nav__item').removeClass('nav__item--active');
+	event.preventDefault();
+	$(this).toggleClass('nav__item--active');
 });

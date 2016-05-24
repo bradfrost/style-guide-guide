9e09f7be3b1864f1f862c433513c5fd3e9525ccc
Ian Frost <inf5000@gmail.com>
2016-05-24 02:16:06 -0400

    Switched About md file to html File. Added updates page. Tweaked header

--- js/menu.js
+++ js/menu.js
@@ -3,8 +3,13 @@ $('.nav__trigger').click(function(event) {
 	$('.nav__list').toggleClass('nav__list--active');
 });
 
-$('.nav__item').click(function(event) {
+$('.content').click(function(event) {
+	$('.nav__list').removeClass('nav__list--active');
 	$('.nav__item').removeClass('nav__item--active');
+});
+
+$('.nav__item').click(function(event) {
 	event.preventDefault();
+	$('.nav__item').removeClass('nav__item--active');
 	$(this).toggleClass('nav__item--active');
 });

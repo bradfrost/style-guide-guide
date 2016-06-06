cc5f3b5c323360135a1d69a22bb20ffbc56a5ccf
Ian Frost <inf5000@gmail.com>
2016-06-06 18:35:20 -0400

    Reformatted the JS to act more like an accordion

--- js/menu.js
+++ js/menu.js
@@ -3,15 +3,9 @@ $('.c-nav__trigger').click(function(event) {
 	$('.c-nav').toggleClass('c-nav--is-active');
 });
 
-$('.content').click(function(event) {
-	$('.c-nav').removeClass('c-nav--is-active');
-	$('.c-nav__item').removeClass('c-nav__item--is-active');
-});
-
 $('.js-c-nav__link').click(function(event) {
 	if($(this).parent('.c-nav__item').children('.c-nav__sublist').hasClass('c-nav__sublist--has-children')) {
 	event.preventDefault();
-	$('.c-nav__item').removeClass('c-nav__item--is-active');
 	$(this).parent('.c-nav__item').toggleClass('c-nav__item--is-active');
 	}
 });

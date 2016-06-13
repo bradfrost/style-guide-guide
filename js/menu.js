44674949ff72f58693ca830ada156cb0134b81d9
Ian Frost <inf5000@gmail.com>
2016-06-13 16:19:05 -0400

    Added pseudo element triangles after nav items with children

--- js/menu.js
+++ js/menu.js
@@ -6,6 +6,6 @@ $('.c-nav__trigger').click(function(event) {
 $('.js-nav__link').click(function(event) {
 	if($(this).parent('.c-nav__item').children('.c-nav__sublist').hasClass('c-nav__sublist--has-children')) {
 	event.preventDefault();
-	$(this).parent('.c-nav__item').toggleClass('c-nav__item--is-active');
+	$(this).parent('.c-nav__item').toggleClass('c-nav__item--has-children--is-active');
 	}
 });

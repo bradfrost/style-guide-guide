a02ada1cd02ee0021df1208caa8f65b14280231a
Ian Frost <inf5000@gmail.com>
2016-06-16 18:06:13 -0400

    Added spaces between divs. Added comments to JS

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,7 @@
+
+/**
+ * Add Active State to Current Page(and parent link if it is a sublist page) in Nav 
+ */
 $(function() {
      var pgurl = window.location.href.replace(/^.*\/\/[^\/]+/, '');
      $(".c-nav__link").each(function(){
@@ -10,6 +14,9 @@ $(function() {
      })
 });
 
+/**
+ * Open and Close Nav With Nav Trigger
+ */
 $('.c-nav__trigger').click(function(event) {
 	var trigger = $('.c-nav__trigger');
 	event.preventDefault();
@@ -25,11 +32,17 @@ $('.c-nav__trigger').click(function(event) {
     }
 });
 
+/**
+ * Close Nav on Small Screens by Clicking Anywhere Within the .content Portion of the Page 
+ */
 $('.content').click(function(event) {
 	event.preventDefault();
 	$('.c-nav').removeClass('c-nav--is-active');	
 });
 
+/**
+ * Expand Sublist In Nav By Adding Active Class to Parent List Item
+ */
 $('.js-nav__link').click(function(event) {
 	if($(this).parent('.c-nav__item').children('.c-nav__sublist').hasClass('c-nav__sublist--has-children')) {
 	event.preventDefault();
